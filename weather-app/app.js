f44c261d8e08b3741cce9b70b511d78b7a16b431
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2022-10-08 12:02:58 +0100

    degree sign

--- weather-app/app.js
+++ weather-app/app.js
@@ -38,10 +38,10 @@ window.addEventListener('load', ()=> {
                     //Change temperature to Celsius/Fareheit
                     temperatureSection.addEventListener('click', ()=>{
                         if(temperatureSpan.textContent === 'C') {
-                            temperatureSpan.textContent = 'F';
+                            temperatureSpan.textContent = '\u00B0'+'F';
                             temperatureDegree.textContent = Math.floor(celsius);
                         }else{
-                            temperatureSpan.textContent = 'C';
+                            temperatureSpan.textContent = '\u00B0'+'C';
                             temperatureDegree.textContent = temp;
                         }
                     });
